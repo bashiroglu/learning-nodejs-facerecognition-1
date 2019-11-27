193ed9bb3bbff25a6e6e56b345be70354ef91b2b
unknown <abdullabashir32@gmail.com>
2019-11-27 11:34:13 +0100

    added post image and prodile if

---
+++ readme.md
@@ -0,0 +1,18 @@
+This is learning based project which I practise some basic React features. The idea of this project was taken from Andrei Neagoie's Udemy course called [ Zero to Mastery](https://www.udemy.com/course/the-complete-web-developer-zero-to-mastery/) and followed along with videos and added some extra feature.
+
+The name of project: learning-nodejs-facerecognition-1
+
+Used Languages and concepts:
+
+1. express
+2. app.listen
+3. get, post
+4.
+5.
+6.
+7.
+8.
+
+After download project files, in the project directory, you can run :
+
+### `npm start`
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const database = {
@@ -11,19 +11,50 @@ const database = {
       id: '123',
       name: 'orxan',
       email: 'orxan@gmail.com',
-      password: '12345'
+      password: '12345',
+      entries: 0
     },
     {
-      id: '124',
+      id: '125',
       name: 'abdulla',
       email: 'abdulla@gmail.com',
-      password: '123456'
+      password: '123456',
+      entries: 0
     }
   ]
 };
 
 app.get('/', (req, res) => {
   res.send(database.users);
+});
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json('no such user');
+  }
+});
+
+app.post('/image', (req, res) => {
+  const { id } = req.body;
+
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json('no such user');
+  }
 });
 
 app.post('/signin', (req, res) => {
