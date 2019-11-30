2df4c8829d6eb3da373be7ec153421b59f726f9a
unknown <abdullabashir32@gmail.com>
2019-11-30 11:50:52 +0100

    transaction to users table to login and hashing password by using bcrypt

--- server.js
+++ server.js
@@ -97,26 +97,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { name, password, email } = req.body;
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
-    })
-    .then(user => res.json(user[0]))
-    .catch(err => res.status(400).json('unable to register'));
-  // bcrypt.hash(password, null, null, function(err, hash) {
-  //   console.log(hash);
-  // });
-  // database.users.push({
-  //   id: '125',
-  //   name: name,
-  //   email: email,
-  //   entries: 0,
-  //   joined: new Date()
-  // });
-  // res.json(database.users[database.users.length - 1]);
+  const hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(err => res.status(400).json('unable to register'));
 });
 
 app.listen(3001, () => {
