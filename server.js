45b02a373c7a1babe33af8daa7c2b19501d04542
unknown <abdullabashir32@gmail.com>
2019-11-30 11:26:15 +0100

    added put function which increase entires in db by incrementing

--- server.js
+++ server.js
@@ -47,22 +47,22 @@ app.get('/', (req, res) => {
   res.send(database.users);
   // console.log(database.users);
 });
-
-
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json('no such user');
-  }
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  db.select('*')
+    .from('users')
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('could not found');
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'));
 });
+
+
 
 app.post('/signin', (req, res) => {
   // bcrypt.compare(
