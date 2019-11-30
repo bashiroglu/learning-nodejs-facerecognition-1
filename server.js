5959cdc12a93ab0d9c479c23bac7515ee94ac374
unknown <abdullabashir32@gmail.com>
2019-11-30 11:36:22 +0100

    really added put function which increase entires in db by incrementing  and the commit before this one was pofile id function which was bringing real user using id from params

--- server.js
+++ server.js
@@ -62,7 +62,17 @@ app.get('/profile/:id', (req, res) => {
     .catch(err => res.status(400).json('error getting user'));
 });
 
-
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entires'));
+});
 
 app.post('/signin', (req, res) => {
   // bcrypt.compare(
